5072332b85a6091b20c4cb45b5912602c0a1e624
Joshua Mutambuki <mutambukijoshua2@gmail.com>
2024-06-19 12:59:31 +0300

    image 2

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 var randomNumber1 = Math.floor(Math.random() *6 )+ 1;
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
 
-var randomImageSource = "images/" + randomDiceImage;
+var randomImageSource = "images/" + randomDiceImage1;
 
 var image1= document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randomImageSource)
+image1.setAttribute("src", randomImageSource);
+
+//image 2
+
+var randomNumber2 = Math.floor(Math.random() *6) +1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomDiceImage2);
+
+//Comparing the two images
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 won"
+}else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 won"
+}else{
+    document.querySelector("h1").innerHTML = "Draw"
+}
+
+
 
 
 
